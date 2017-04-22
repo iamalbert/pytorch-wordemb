d8e33c0d32d7289be8370e0711840ddf1bb77b20
Wen Li Zhuang <wlzhuang@nlg.csie.ntu.edu.tw>
2017-04-22 21:45:14 +0800

    fix

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ here = path.abspath(path.dirname(__file__))
 import build_ext
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -26,7 +26,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1',
+    version='0.0.2',
 
     description='Load pretrained word embeddings (word2vec, glove format) into torch.FloatTensor for PyTorch',
     long_description=long_description,
@@ -56,7 +56,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=['torchwordemb'], #find_packages(exclude=['contrib', 'docs', 'tests']),
+    packages=find_packages(exclude=['contrib', 'docs', 'tests', 'build']),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
